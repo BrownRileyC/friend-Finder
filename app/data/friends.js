40fa8aa0d8dc7174acc9fd7ba7366fd181e82f5e
Riley Brown <brown.riley.c@gmail.com>
2019-02-19 19:29:45 -0500

    the modal doesnot work yet but it seems to almost be there

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,37 @@
-var friends = {
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-};
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Georgia",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            4,
+            2,
+            3,
+            3,
+            6,
+            4,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
 
 module.exports = {
     friends
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,6 +8,29 @@ app.get('/api/friends', function(req, res){
 });
 
 app.post('/api/friends', function(req, res){
+    console.log('Yo I got posted mang');
+    let newFriend = req.body;
+    console.log(req.body);
+    let bestMatch;
+    let bestMatchDifference = 50;
 
+    for (var i = 0; i < friends.friends.length; i++){
+        console.log(friends.friends[i]);
+        let totalDifference = 0;
+        
+        for (var k = 0; k < friends.friends[i].scores.length; k++){
+            
+            let difference = parseInt(newFriend.scores[k]) - parseInt(friends.friends[i].scores[k]);
+            totalDifference += Math.abs(difference);
+        }
+        if (totalDifference < bestMatchDifference) {
+            bestMatchDifference = totalDifference;
+            console.log('After run '+i+' the best difference was '+bestMatchDifference)
+            bestMatch = friends.friends[i]
+        }
+        console.log(bestMatch);
+        console.log(bestMatchDifference);
+    }
+    res.json(bestMatch);
 })
 };

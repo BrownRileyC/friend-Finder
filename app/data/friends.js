faf68216b888d89aaf88e87a8712a44565d4508a
Riley Brown <brown.riley.c@gmail.com>
2019-02-21 11:55:48 -0500

    changed up the friends list and altered the modal some

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,66 @@
 var friends = [
     {
-        name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "A Dragon",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/9/9a/DnD_Dragon.png",
         scores: [
             5,
-            1,
-            4,
-            4,
-            5,
-            1,
+            3,
+            3,
             2,
+            1,
+            1,
+            5,
             5,
             4,
             1
         ]
     },
     {
-        name: "Georgia",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Manticore",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/6/6c/DnD_Manticore.png",
         scores: [
             4,
-            2,
-            3,
-            3,
-            6,
             4,
             2,
+            3,
+            1,
+            5,
+            3,
             5,
             4,
             1
+        ]
+    },
+    {
+        name: "Dwarf",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/c/cb/DnD_Dwarf.png",
+        scores: [
+            5,
+            5,
+            2,
+            1,
+            5,
+            1,
+            3,
+            5,
+            2,
+            4
+        ]
+    },
+    {
+        name: "Djinn",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/b/b8/DnD_Djinn.png",
+        scores: [
+            5,
+            1,
+            5,
+            1,
+            1,
+            5,
+            5,
+            5,
+            1,
+            4
         ]
     }
 ];
